7b0d9c96d986251339d4c2c03109393bf35c5727
Alejandro <alejandro86mg@gmail.com>
2020-11-30 13:54:27 +0100

    created an error page with a flexbox layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
 var reload = browserSync.reload;
 
-gulp.task('serve', function() {
+gulp.task("serve", function() {
   browserSync.init({
     server: {
       baseDir: "./"
